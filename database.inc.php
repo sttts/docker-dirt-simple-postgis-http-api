b6780a8de1309bd397b178da0593e74d64965a60
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2015-02-14 18:33:12 +0100

    Database connection parameters

--- database.inc.php
+++ database.inc.php
@@ -1,10 +1,11 @@
 <?php
 function pgConnection() {
-	$conn = new PDO ("pgsql:host=" + $_ENV['POSTGRES_HOST'] +
-	 ";dbname=" + $_ENV['POSTGRES_DB'],
-	 $_ENV['POSTGRES_USER'],
-	 $_ENV['POSTGRES_PASSWORD'],
-	 array(PDO::ATTR_PERSISTENT => true));
+	$conn = new PDO("pgsql:host=" . $_SERVER['POSTGRES_HOST'] .
+		";port=" . $_SERVER['POSTGRES_PORT'] .
+		";dbname=" . $_SERVER['POSTGRES_DB'],
+		$_SERVER['POSTGRES_USER'],
+		$_SERVER['POSTGRES_PASSWORD']
+	);
     return $conn;
 }
 ?>
